6f38c24fb25dca6630af3970222ebb7b422fd776
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-10 12:56:52 -0300

    feat: adiciona componente de listagem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { List } from './features/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: List,
+}];
