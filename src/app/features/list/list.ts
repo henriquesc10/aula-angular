f0b524626826fe37e18494bd337bb6df663d2916
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-10 13:34:22 -0300

    feat: adiciona listagem de produtos

--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
+  standalone: true,
   imports: [],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
+
 export class List {
 
+  products: any[] = [];
+
+  httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products').subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
