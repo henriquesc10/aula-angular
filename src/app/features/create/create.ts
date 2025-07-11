90f4f2f373fabf4081774da550dcabdee4b64d8a
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-11 12:00:10 -0300

    feat: adiciona campos ao formulario de criacao de produto

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,11 +1,24 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.html',
   styleUrl: './create.css'
 })
 export class Create {
+  form = new FormGroup({
+    title: new FormControl<string>('', { nonNullable: true, validators: Validators.required, }),
+  })
 
-}
+  onSubmit() {
+    this.form.controls.title.value;
+  };
+ 
+} // Fechamento class Create
