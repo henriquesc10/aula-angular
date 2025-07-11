285bcd6414e2d3ce070b20bde7cbcf3ff84289b6
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-11 11:01:54 -0300

    feat: adiciona roteamento para CreateComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { List } from './features/list/list';
+import { Create } from './features/create/create';
 
 export const routes: Routes = [{
     path: '',
     component: List,
-}];
+},
+{
+    path: 'create-product',
+    component: Create,
+}
+];
