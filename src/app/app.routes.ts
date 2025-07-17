6a4f11bb3ac5ad0d3c6c76efe8b5fa02e2300a93
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-17 12:02:10 -0300

    refactor: cria funções resolver especializadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
-import { ActivatedRouteSnapshot, RedirectCommand, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { List } from './features/list/list';
-import { Create } from './features/create/create';
-import { Observable } from 'rxjs';
-import { ProductsService } from './shared/services/products.service';
-import { inject } from '@angular/core';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
 export const routes: Routes = [
     {
         path: '',
         resolve: {
             produtcs: () => {
-                const productsService = inject(ProductsService);
-                return productsService.getAll();
+                products: getProducts
             }
         },
         component: List,
@@ -23,10 +20,7 @@ export const routes: Routes = [
     {
         path: 'edit-product/:id',
         resolve: {
-            product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-                const productsService = inject(ProductsService)
-                return productsService.get(route.paramMap.get('id') as string);
-            },
+            product: getProduct,
         },
         loadComponent: () => import('./features/edit/edit').then(m => m.Edit),
     },
